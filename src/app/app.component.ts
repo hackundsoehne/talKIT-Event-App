f4ce799663a38f4f2cbe8efc3bbc6b478e16f9c0
LeanderK <Leander.Kurscheidt@gmx.de>
2018-04-22 18:02:45 +0200

    disabled cordova

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
-import { Platform } from 'ionic-angular';
-import { StatusBar } from '@ionic-native/status-bar';
-import { SplashScreen } from '@ionic-native/splash-screen';
+// import { Platform } from 'ionic-angular';
+// import { StatusBar } from '@ionic-native/status-bar';
+// import { SplashScreen } from '@ionic-native/splash-screen';
 
 
 
@@ -16,13 +16,15 @@ export class MyApp {
   
   rootPage:any = TabsControllerPage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
-    platform.ready().then(() => {
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-      statusBar.styleDefault();
-      splashScreen.hide();
-    });
-  }
+  constructor() {}
+
+  // constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  //   platform.ready().then(() => {
+  //     // Okay, so the platform is ready and our plugins are available.
+  //     // Here you can do any higher level native things you might need.
+  //     statusBar.styleDefault();
+  //     splashScreen.hide();
+  //   });
+  // }
   
 }
